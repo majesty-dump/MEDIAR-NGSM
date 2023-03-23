c9fef45b88800ccc66a830cb3e22b31bd29b9bed
majesty-dump <sky92033@live.com>
2023-03-23 09:17:06 +0300

    [REF] classification loss settings

--- core/MEDIAR/Trainer.py
+++ core/MEDIAR/Trainer.py
@@ -51,7 +51,7 @@ class Trainer(BaseTrainer):
         
         self.bce_loss = nn.BCEWithLogitsLoss(reduction="mean")
         self.mse_loss = nn.MSELoss(reduction="mean")
-        self.dice_loss = smp.losses.DiceLoss(mode="multiclass", from_logits=True, ignore_index=4)
+        self.dice_loss = smp.losses.DiceLoss(mode="multiclass", from_logits=True, ignore_index=UNDEFINED_LABEL_INDEX)
 
         self.MediarLoss = namedtuple('MediarLoss', ['bce_loss', 'mse_loss', 'dice_loss'])
 
